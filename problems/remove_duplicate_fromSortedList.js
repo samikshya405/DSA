389801ab6ca70e505755c42194fb5e19c15556ab
samikshya <samikshyakharel405@gmail.com>
2024-05-30 13:34:31 +1000

    remove duplicate from sorted list

--- problems/remove_duplicate_fromSortedList.js
+++ problems/remove_duplicate_fromSortedList.js
@@ -49,10 +49,9 @@ const arrToLinked = (arr, list) => {
 };
 
 const l1 = new LinkedList();
-const givenArr = [1, 1, 2, 3, 3, 4, 4, 5];
+const givenArr = [1, 1];
 arrToLinked(givenArr, l1);
 l1.print();
-const l2 = new LinkedList();
 
 var deleteDuplicates = function (head) {
   if (!head || !head.next) {
@@ -60,19 +59,62 @@ var deleteDuplicates = function (head) {
   }
   let dummy = new Node(0);
   dummy.next = head;
-  let current = dummy;
 
-  while (current.next && current.next.next) {
-    if (current.next.data === current.next.next.data) {
-      let duplicateValue = current.next.data;
-      while (current.next && current.next.data === duplicateValue) {
-        current.next = current.next.next;
+  let prev = dummy;
+  let current = head;
+  while (current) {
+    if (current.data === current.next.data) {
+      while (current.next && current.data === current.next.data) {
+        current = current.next;
       }
+      prev.next = current.next;
+      current = current.next;
     } else {
+      prev = current;
       current = current.next;
     }
   }
   return dummy.next;
 };
-l2.head = deleteDuplicates(l1.head);
+l1.head = deleteDuplicates(l1.head);
+
+l1.print();
+
+// 83. Remove Duplicates from Sorted List
+// Solved
+// Easy
+// Topics
+// Companies
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Example 1:
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+
+const l2 = new LinkedList();
+const ll = [1, 1, 2, 3, 4, 4, 5, 6, 6];
+arrToLinked(ll, l2);
+const removeDuplicate = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+  const dummyNode = new Node(0);
+  dummyNode.next = head;
+  let current = head;
+  let prev = dummyNode;
+  while (current && current.next) {
+    if (current.data === current.next.data) {
+      while (current.next && current.data === current.next.data) {
+        current = current.next;
+      }
+      prev.next = current;
+    } else {
+      prev = current;
+    }
+    current = current.next;
+  }
+  return dummyNode.next;
+};
+l2.head = removeDuplicate(l2.head);
 l2.print();
