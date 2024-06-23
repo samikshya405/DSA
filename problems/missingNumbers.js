fc14b4ebb58bc80ba4b7666a6f123bfc2276c6ca
samikshya <samikshyakharel405@gmail.com>
2024-06-23 23:15:30 +1000

    missing number

---
+++ problems/missingNumbers.js
@@ -0,0 +1,5 @@
+// 2. **Find the Missing Number**
+//    - **Question:** Given an array containing `n` distinct numbers taken from the range `0` to `n`, find the missing number.
+//    - **Expected Input:** `findMissingNumber([3, 0, 1])`
+//    - **Expected Output:** `2`
+
